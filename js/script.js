8d9659689ef9b8d766f747208dca6ddb57becbbf
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2025-01-11 00:52:17 +0200

    en

--- js/script.js
+++ js/script.js
@@ -9,9 +9,26 @@ $(document).ready(function () {
     }
   });
 
-
-  
+  $(".owl-suggestions").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    rtl: true,
+       navText: [
+      '<i class="fas fa-chevron-right"></i>', // Font Awesome right arrow
+        '<i class="fas fa-chevron-left"></i>' // Font Awesome left arrow
+    ],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    },
 });
 
-
- 
+});
