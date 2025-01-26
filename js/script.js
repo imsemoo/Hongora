38eaaf3a03365fd2e43b3986a5733eb68dd4654a
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2025-01-26 23:58:30 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   // عند التمرير
   $(window).on("scroll", function () {
-    // التحقق إذا كان التمرير قد تجاوز 50 بيكسل (يمكنك تغيير القيمة)
     if ($(window).scrollTop() > 50) {
       $(".navbar").addClass("fixed-nav");
     } else {
